59b86457712d89d19e16aef5ae5ee465633effb9
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-11-19 18:22:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Routes from './routes';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render(){
+    return(
+      <div>
+         <Routes/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            
+            <Switch>
+                
+            </Switch>
+            
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
