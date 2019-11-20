4a6326b9e088422876aa116046b1e22fd4d6ed40
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-11-19 21:18:54 -0300

    iniciando API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,26 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+        filmes: []
     }
+
+    this.loadFilmes = this.loadFilmes.bind(this);
+  }
+
+  componentDidMount(){
+    this.loadFilmes();
+  }
+
+  loadFilmes(){
+    // Url da API
+    let url = 'https://sujeitoprogramador.com/r-api/?api=filmes'
+
+    fetch(url)
+    .then((r)=> r.json())
+    .then((json) =>{
+        this.setState({filmes: json});
+        console.log(json);
+    });
   }
 
   render(){
