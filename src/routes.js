c8c5bf1b6a31ebc00dd0fc0ceea054a5e02423f1
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-11-19 23:38:10 -0300

    pagina de erro

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home/'
 import Header from './components/Header';
+import Erro from './pages/Erro';
 
 
 const Routes = () => {
@@ -10,6 +11,7 @@ const Routes = () => {
             <Header/>
             <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route path="*" component={Erro}/>
             </Switch>
             
         </BrowserRouter>
