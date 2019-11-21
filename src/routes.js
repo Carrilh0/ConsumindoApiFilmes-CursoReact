c41ec121e5e0f532b50141927ead8b2c39e4c5f7
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-11-21 02:50:54 -0300

    exemplo para rota autenticada

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home/';
 import Erro from './pages/Erro';
 import Filme from './pages/Filme';
+import { autenticado } from './auth';
 
+const PrivateRoute = ({ component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        autenticado() ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{pathname: '/', state: {from: props.location}}} />
+        )
+    )} />
+);
 
 const Routes = () => {
     return(
@@ -12,7 +22,7 @@ const Routes = () => {
             <Header/>
             <Switch>
                 <Route exact path="/" component={Home}/>
-                <Route exact path="/filme/:id" component={Filme}/>
+                <PrivateRoute exact path="/filme/:id" component={Filme}/>
                 <Route path="*" component={Erro}/>
             </Switch>
             
